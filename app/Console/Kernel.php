c89b0223b909dbb1661669f4b76c9676e1a39acd
tomb <tomb@saturnads.net>
2025-08-17 09:41:56 +0700

    init project

---
+++ app/Console/Kernel.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Console;
+
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+
+class Kernel extends ConsoleKernel
+{
+    /**
+     * Define the application's command schedule.
+     */
+    protected function schedule(Schedule $schedule): void
+    {
+        $schedule->command('xxvn:daily-movies')->hourly();
+         $schedule->command('movies:bump-updated --limit=5')->everyThirtyMinutes();
+    }
+
+    /**
+     * Register the commands for the application.
+     */
+    protected function commands(): void
+    {
+        $this->load(__DIR__.'/Commands');
+
+        require base_path('routes/console.php');
+    }
+}
